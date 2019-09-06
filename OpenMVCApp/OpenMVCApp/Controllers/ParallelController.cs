34703142bd13711d7107e3240adb1afa4584f6ec
Paul Wang <1043469124@qq.com>
2019-09-06 23:35:26 +0800

    Async&sync

--- OpenMVCApp/OpenMVCApp/Controllers/ParallelController.cs
+++ OpenMVCApp/OpenMVCApp/Controllers/ParallelController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.IO;
 using System.Security.Cryptography;
+using System.Net;
 
 namespace OpenMVCApp.Controllers
 {
@@ -41,7 +42,71 @@ namespace OpenMVCApp.Controllers
             return View();
         }
 
+        public ActionResult executeClick_sync()
+        {
+            var timer3 = new Stopwatch();
+            timer3.Start();
 
+            RunDownloadWebSync();
+
+            timer3.Stop();
+
+            ViewData["timer3"] += timer3.ElapsedMilliseconds.ToString();
+
+            return View();
+        }
+
+        private void RunDownloadWebSync()
+        {
+            List<String> websiteCollection = getWebSite();
+
+            foreach (var ws in websiteCollection)
+            {
+                WebSiteDataModel wsd = getWebSiteData(ws);
+                ReportWebSiteInfo(wsd);
+            }
+        }
+
+        private WebSiteDataModel getWebSiteData(String ws)
+        {
+            WebSiteDataModel wdsm = new WebSiteDataModel();
+
+            WebClient wc = new WebClient();
+            wdsm.WebSiteUri = ws;
+            wdsm.WebSiteData = wc.DownloadString(new Uri(ws));
+
+            return wdsm;
+        }
+
+        public void ReportWebSiteInfo(WebSiteDataModel wsd)
+        {
+            ViewData["timer3"] += $"{wsd.WebSiteUri} downloaded :{wsd.WebSiteData.Length} characters long.{Environment.NewLine}";
+        }
+        private List<String> getWebSite()
+        {
+            List<String> ls = new List<string>();
+            ls.Add("https://www.baidu.com");
+            ls.Add("https://www.google.com");
+            ls.Add("https://www.Microsoft.com");
+            return ls;
+        }
+
+        private static void RunDownloadWebAsync()
+        {
+
+        }
+
+        private void executeClick_Async()
+        {
+            var timer3 = new Stopwatch();
+            timer3.Start();
+
+            RunDownloadWebAsync();
+
+            timer3.Stop();
+
+            ViewData["timer3"] = timer3.Elapsed;
+        }
 
         public async static Task<String> Encrypt(String input = "TestTestTestTestTestTestTestTestTestTestTestTestTestTestTest")
         {
@@ -67,5 +132,12 @@ namespace OpenMVCApp.Controllers
             }
             return Value;
         }
+    }
+    public class WebSiteDataModel
+    {
+        public String WebSiteUri { get; set; }
+
+        public String WebSiteData { get; set; }
+
     }
 }
